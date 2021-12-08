17af8e1d9296e796d59c6be6ca876b99d23cfee6
Vipin Gupta <smtutsofficial@gmail.com>
2021-12-08 19:15:48 +0530

    new application

--- android/app/src/main/kotlin/com/example/testwala/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.testwala
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ testtution/android/app/src/main/kotlin/com/example/testtution/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testtution
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
